d88459a5b931da9c6f8600dda7581a83c1a1b9a6
Zendy <nguyenduc.051194@gmail.com>
2021-08-25 21:39:36 +0700

    User implement MutationResponse

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,20 +1,32 @@
-import { User } from "../entities/User";
 import argon2 from "argon2";
 import { Arg, Mutation, Resolver } from "type-graphql";
+import { User } from "../entities/User";
+import { UserMutationResponse } from "./../types/UserMutationResponse";
 
 @Resolver()
 export class UserResolver {
-  @Mutation((_returns) => User, { nullable: true })
+  @Mutation((_returns) => UserMutationResponse, { nullable: true })
   async register(
     @Arg("email") email: string,
     @Arg("username") username: string,
     @Arg("password") password: string
-  ): Promise<User | null> {
+  ): Promise<UserMutationResponse> {
     try {
       const existingUser = await User.findOne({
         where: [{ username }, { email }],
       });
-      if (existingUser) return null;
+      if (existingUser)
+        return {
+          code: 400,
+          success: false,
+          message: "Duplicated username or email",
+          errors: [
+            {
+              field: existingUser.username === username ? "username" : "email",
+              message: `${existingUser.username === username ? "Username" : "Email"} already taken`,
+            },
+          ],
+        };
 
       const hashPassword = await argon2.hash(password);
 
@@ -23,11 +35,26 @@ export class UserResolver {
         password: hashPassword,
         email,
       });
-
-      return await User.save(newUser);
+      
+      return {
+        code: 200,
+        success: true,
+        message: 'User registration successful',
+        user: await User.save(newUser)
+      }
     } catch (error) {
       console.log(error);
-      return null;
+      return {
+        code: 500,
+        success: false,
+        message: "Duplicated username or email",
+        errors: [
+          {
+            field: `Internal server error ${error.message}`,
+            message: "Username or email already taken",
+          },
+        ],
+      };
     }
   }
 }
---
+++ server/src/types/UserMutationResponse.ts
@@ -0,0 +1,17 @@
+import { FieldError } from "./FieldError";
+import { User } from "../entities/User";
+import { Field, ObjectType } from "type-graphql";
+import { IMutationResponse } from "./MutationResponse";
+
+@ObjectType({ implements: IMutationResponse })
+export class UserMutationResponse implements IMutationResponse {
+  code: number;
+  success: boolean;
+  message?: string;
+
+  @Field({ nullable: true })
+  user?: User;
+
+  @Field((_type) => [FieldError], { nullable: true })
+  errors?: FieldError[];
+}

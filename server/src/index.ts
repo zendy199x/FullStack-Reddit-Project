6bc4bd74cf4ff5f55281c511cf4eeec94d33b82e
Zendy <nguyenduc.051194@gmail.com>
2021-08-25 23:31:03 +0700

    Connect MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,19 @@
 require("dotenv").config();
-import "reflect-metadata";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { Context } from "apollo-server-core/dist/types";
+import { ApolloServer } from "apollo-server-express";
+import MongoStore from "connect-mongo";
 import express from "express";
+import session from "express-session";
+import mongoose from "mongoose";
+import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import { ApolloServer } from "apollo-server-express";
+import { User } from "./entities/User";
 import { HelloResolver } from "./resolvers/hello";
 import { UserResolver } from "./resolvers/user";
-import { Context } from "apollo-server-core/dist/types";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import { User } from "./entities/User";
 
 const main = async () => {
   await createConnection({
@@ -24,8 +28,39 @@ const main = async () => {
 
   const app = express();
 
+  // Session/Cookie store
+  const mongoUrl = `mongodb+srv://${process.env.SESSION_DB_USERNAME_DEV_PROD}:${process.env.SESSION_DB_PASSWORD_DEV_PROD}@reddit.msu4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+  await mongoose.connect(mongoUrl, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+
+  console.log(`MongoDB Connected`);
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: MongoStore.create({ mongoUrl }),
+      cookie: {
+        maxAge: 1000 * 60 * 60, // 1 hour
+        httpOnly: true, // JS Front end cannot access the cookie
+        secure: __prod__, // Cookie only works in https
+        sameSite: "lax", // Protect against CSRF
+      },
+      secret: process.env.SESSION_SECRET_DEV_PROD as string,
+      saveUninitialized: false, // Don't save empty sessions, right from the start
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
-    schema: await buildSchema({ resolvers: [HelloResolver, UserResolver], validate: false }),
+    schema: await buildSchema({
+      resolvers: [HelloResolver, UserResolver],
+      validate: false,
+    }),
     context: ({ req, res }): Context => ({ req, res }),
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
   });

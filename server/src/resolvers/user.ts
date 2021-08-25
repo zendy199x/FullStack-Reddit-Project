4947b4581b02f5a32cb89db02bad4842499c4bf7
Zendy <nguyenduc.051194@gmail.com>
2021-08-25 22:40:19 +0700

    Login mutation resolver

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,13 +1,14 @@
 import argon2 from "argon2";
 import { Arg, Mutation, Resolver } from "type-graphql";
 import { User } from "../entities/User";
+import { LoginInput } from "../types/LoginInput";
 import { RegisterInput } from "../types/RegisterInput";
 import { UserMutationResponse } from "./../types/UserMutationResponse";
 import { validateRegisterInput } from "./../utils/validateRegisterInput";
 
 @Resolver()
 export class UserResolver {
-  @Mutation((_returns) => UserMutationResponse, { nullable: true })
+  @Mutation((_return) => UserMutationResponse, { nullable: true })
   async register(
     @Arg("registerInput") registerInput: RegisterInput
   ): Promise<UserMutationResponse> {
@@ -53,6 +54,59 @@ export class UserResolver {
         success: true,
         message: "User registration successful",
         user: await User.save(newUser),
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        code: 500,
+        success: false,
+        message: `Internal server error ${error.message}`,
+      };
+    }
+  }
+
+  @Mutation((_return) => UserMutationResponse)
+  async login(
+    @Arg("loginInput") { usernameOrEmail, password }: LoginInput
+  ): Promise<UserMutationResponse> {
+    try {
+      const existingUser = await User.findOne(
+        usernameOrEmail.includes("@")
+          ? { email: usernameOrEmail }
+          : { username: usernameOrEmail }
+      );
+
+      if (!existingUser)
+        return {
+          code: 400,
+          success: false,
+          message: "User not found",
+          errors: [
+            {
+              field: "usernameOrEmail",
+              message: "Username or email incorrect",
+            },
+          ],
+        };
+
+      const passwordValid = await argon2.verify(
+        existingUser.password,
+        password
+      );
+
+      if (!passwordValid)
+        return {
+          code: 400,
+          success: false,
+          message: "Wrong password",
+          errors: [{ field: "password", message: "Wrong password" }],
+        };
+
+      return {
+        code: 200,
+        success: true,
+        message: "Logged in successfully",
+        user: existingUser,
       };
     } catch (error) {
       console.log(error);
